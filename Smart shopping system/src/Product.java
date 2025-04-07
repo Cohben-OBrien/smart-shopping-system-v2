9ff61dc36515ae950f54701a6f0b13afb2e37c60
Jose Castro <Joserocio1572@outlook.es>
2025-04-07 14:10:03 +0100

    Add files via upload

--- Smart shopping system/src/Product.java
+++ Smart shopping system/src/Product.java
@@ -1,36 +1,47 @@
-import java.awt.desktop.PreferencesEvent;
+package smartshop;
 
 public class Product {
-    String name;
-    float price;
-    int quantity;
+    private String name;   // Name of the product
+    private float price;   // Price in pounds
+    private int quantity;  // Quantity available in stock
 
+    // Constructor
     public Product(String name, float price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
+    // Method to sell a product and update quantity
+    public void sell(int amount) {
+        if (this.quantity >= amount) {
+            this.quantity -= amount;
+        } else {
+            System.out.println("Not enough stock!");
+        }
+    }
 
-    public void sale() {
-        this.quantity--;
+    // Method to restock the product
+    public void restock(int amount) {
+        this.quantity += amount;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
     }
 
     public float getPrice() {
-        return this.price;
+        return price;
     }
 
     public int getQuantity() {
-        return this.quantity;
+        return quantity;
     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-
 }
+
+
+
+
+
+
